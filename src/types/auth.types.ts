17f326c72abb97227ef989b3224047f7791698a0
Samuel Souza <samuel.souzagomes001@gmail.com>
2023-08-07 19:58:38 -0300

    feat: finish auth module

--- src/types/auth.types.ts
+++ src/types/auth.types.ts
@@ -1,10 +1,13 @@
 import { z } from 'zod';
-import { authValidation, checkVerificationValidation, createVerificationValidation } from '~/validations/auth.validation';
+import { authValidation, changePasswordValidation, checkVerificationValidation, createVerificationValidation, forgotPasswordValidation, resetPasswordValidation } from '~/validations/auth.validation';
 
 export namespace AuthTypes {
 	export type AuthParams = z.infer<typeof authValidation>;
 	export type CreateVerificationParams = z.infer<typeof createVerificationValidation>;
   export type CheckVerificationParams = z.infer<typeof checkVerificationValidation>;
+	export type ForgotPasswordParams = z.infer<typeof forgotPasswordValidation>;
+	export type ChangePasswordParams = z.infer<typeof changePasswordValidation>;
+	export type ResetPasswordParams = z.infer<typeof resetPasswordValidation>;
 	export enum VerificationStatus {
 		UNVERIFIED = 'unverified',
 		VERIFIED = 'verified',
--- src/validations/auth.validation.ts
+++ src/validations/auth.validation.ts
@@ -23,3 +23,26 @@ export const checkVerificationValidation = z
 		client_info: z.string(),
 	})
 	.strict();
+
+export const forgotPasswordValidation = z
+	.object({
+		email: z.string().email("EMAIL_INVALID").endsWith('@crm.com', 'NOT_CRM_MAIL_VALID'),
+		callbackUrl: z.string().url("CALLBACK_URL_INVALID"),
+	})
+	.strict();
+
+export const changePasswordValidation = z
+	.object({
+		id: z.number().int("ID_INVALID"),
+		old_password: z.string().min(8, "PASSWORD_MIN_LENGTH"),
+		password: z.string().min(8, "PASSWORD_MIN_LENGTH"),
+		pin: z.string().optional()
+	})
+	.strict();
+
+export const resetPasswordValidation = z
+	.object({
+		token: z.string().uuid('TOKEN_INVALID'),
+		password: z.string().min(8, "PASSWORD_MIN_LENGTH").max(32, "PASSWORD_MAX_LENGTH"),
+	})
+	.strict();
