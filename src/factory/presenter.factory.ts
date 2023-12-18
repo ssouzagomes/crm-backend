a63586467192c3e661e14183f5a68bbd0973795f
Samuel Souza <ssouza.gomes10@gmail.com>
2023-12-18 20:55:12 -0300

    fix: change API response 'data' to 'result'

--- src/factory/presenter.factory.ts
+++ src/factory/presenter.factory.ts
@@ -1,12 +1,12 @@
 export class PresenterFactory<T> {
-	public readonly data: T;
+	public readonly result: T;
 
 	public readonly message?: string[];
 
 	public readonly isValid: boolean;
 
-	constructor(data: T, isValid: boolean, message?: string[]) {
-		this.data = data;
+	constructor(result: T, isValid: boolean, message?: string[]) {
+		this.result = result;
 		this.message = message;
 		this.isValid = isValid;
 	}
